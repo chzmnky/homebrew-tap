ba982ef0c891d35bf5c6daa95470b0a338b39b84
Robin Bolton <robin@robinbolton.com>
2020-06-25 03:27:00 -0600

    Update HL7Tools.rb

--- Formula/HL7Tools.rb
+++ Formula/HL7Tools.rb
@@ -7,11 +7,11 @@ class HL7Tools < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/chzmnky/HL7Tools/releases/download/v0.1.0/HL7Tools_0.1.0_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy.
+    url "https://github.com/chzmnky/HL7Tools/releases/download/v0.1.0/HL7Tools_0.1.0_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy
     sha256 "13fcb1acca60d85cd78f830d8162bd78575dff67ab1b97582ecab0dff5ef12da"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/chzmnky/HL7Tools/releases/download/v0.1.0/HL7Tools_0.1.0_Linux_x86_64.tar.gz", :using => CurlDownloadStrategy.
+      url "https://github.com/chzmnky/HL7Tools/releases/download/v0.1.0/HL7Tools_0.1.0_Linux_x86_64.tar.gz", :using => CurlDownloadStrategy
       sha256 "de5a02690cfe61175550ce607308de40a1f30999a2c59d9e3280cff0af8ea13b"
     end
   end
