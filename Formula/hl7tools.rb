8b6c3265834b5862fce004cf54e61d3a13d82651
Robin Bolton <robin@robinbolton.com>
2020-06-25 16:50:07 -0600

    Update hl7tools.rb

--- Formula/hl7tools.rb
+++ Formula/hl7tools.rb
@@ -8,11 +8,11 @@ class Hl7tools < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/chzmnky/HL7Tools/releases/download/v0.1.0/HL7Tools_0.1.0_Darwin_x86_64.tar.gz", :using => :github_private_release
+    url "https://github.com/chzmnky/HL7Tools/releases/download/v0.1.0/HL7Tools_0.1.0_Darwin_x86_64.tar.gz", :using => :GitHubPrivateRepositoryReleaseDownloadStrategy
     sha256 "3adf5172ba0f90a15aa4dcd242ac5f8b8c60428666f35988d2e6d8a480400199"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/chzmnky/HL7Tools/releases/download/v0.1.0/HL7Tools_0.1.0_Linux_x86_64.tar.gz", :using => :github_private_release
+      url "https://github.com/chzmnky/HL7Tools/releases/download/v0.1.0/HL7Tools_0.1.0_Linux_x86_64.tar.gz", :using => :GitHubPrivateRepositoryReleaseDownloadStrategy
       sha256 "ca156ab40f76ea132bc04ea9c5233f533984731cf0537e30ec951f125107c635"
     end
   end
